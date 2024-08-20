640e9470227bd65300b3a19f08b83778a7f927ac
Abhishek Patil <abishkpatil@gmail.com>
2024-08-20 18:24:45 +0530

    fix: minor text

--- src/components/Working.jsx
+++ src/components/Working.jsx
@@ -4,7 +4,7 @@ const Working = () => {
         <div className="text-left text-xl gap-y-8 flex flex-wrap justify-center items-center mt-8 text-gray-500 sm:justify-between">
             <ol className="space-y-6 text-gray-500 list-decimal list-inside dark:text-gray-400">
                 <li>
-                    <span className="font-semibold text-gray-900 dark:text-white">Sign up on <a href="https://composio.dev/" className="underline underline-offset-2">Composio</a></span> and link your Gmail for seamless integration
+                    <span className="font-semibold text-gray-900 dark:text-white">Sign up on <a href="https://composio.dev/" className="underline underline-offset-2">Composio</a></span> and link your Gmail & Sheets
                 </li>
                 <li>
                     <span className="font-semibold text-gray-900 dark:text-white">Enter keywords</span> you want to search for in your emails
