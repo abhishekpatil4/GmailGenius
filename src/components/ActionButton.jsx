9f659647fa6802992cdd6a001e167f6ec4ba6a0b
Abhishek Patil <abishkpatil@gmail.com>
2024-08-21 14:49:19 +0530

    refactor: minor code refactoring

--- src/components/ActionButton.jsx
+++ src/components/ActionButton.jsx
@@ -1,8 +1,9 @@
+import {Link} from "react-router-dom";
 const ActionButton = ({displayName, link}) => {
-    return <a href={link} className=" bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
+    return <Link to="/dashboard" className=" bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:outline-none focus:ring-purple-300 inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-800">
         {displayName}
         <svg className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-    </a>
+    </Link>
 }
 
 export default ActionButton;
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,9 +1,9 @@
-import FetchStore from "../components/FetchStore";
+import Fetch from "../components/Fetch";
 const Dashboard = () => {
     return <section className="bg-white dark:bg-gray-900 mt-12">
         <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
             <span className="font-semibold text-3xl text-gray-900">Enter Keywords (Crisp & Concise)</span>
-            <FetchStore />
+            <Fetch />
         </div>
     </section>
 }
