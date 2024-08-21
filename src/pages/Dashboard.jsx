4140d3d406ee82219fd3062281b40a54b59e1b4e
Abhishek Patil <abishkpatil@gmail.com>
2024-08-21 12:10:50 +0530

    feat: add form to submit keywords

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,5 +1,11 @@
+import FetchStore from "../components/FetchStore";
 const Dashboard = () => {
-    return <h1>Dashboard</h1>
+    return <section className="bg-white dark:bg-gray-900 mt-12">
+        <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
+            <span className="font-semibold text-3xl text-gray-900">Enter Keywords (Crisp & Concise)</span>
+            <FetchStore />
+        </div>
+    </section>
 }
 
 export default Dashboard;
