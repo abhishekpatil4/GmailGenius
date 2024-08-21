604a5c057521441d9608e2e3a5a7245869243620
Abhishek Patil <abishkpatil@gmail.com>
2024-08-21 20:07:14 +0530

    feat: add ScrollToTop component | fix minor issues

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
+import ScrollToTop from "./components/ScrollToTop";
 
 const App = () => {
   return <>
     <Navbar />
     <BrowserRouter>
+    <ScrollToTop />
       <Routes>
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/" element={<Home />}>
