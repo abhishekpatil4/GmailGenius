513a4f419d90ac2a9985357691fa47d132632bab
Abhishek Patil <abishkpatil@gmail.com>
2024-08-24 03:25:37 +0530

    fix: add sheetId to post request

--- src/components/Fetch.jsx
+++ src/components/Fetch.jsx
@@ -4,13 +4,15 @@ import axios from 'axios';
 const Fetch = () => {
     const [emailKeywords, setEmailKeywords] = useState('');
     const [attributes, setAttributes] = useState('');
+    const [sheetId, setSheetId] = useState('');
 
     const handleFetch = async (e) => {
         e.preventDefault();
         try {
             const response = await axios.post('http://localhost:8000/fetch', {
                 emailKeywords,
-                attributes
+                attributes,
+                sheetId
             });
             console.log('Response:', response.data);
         } catch (error) {
@@ -19,7 +21,17 @@ const Fetch = () => {
     }
 
     return (
-        <form onSubmit={handleFetch} className="max-w-sm mx-auto my-24">
+        <form onSubmit={handleFetch} className="max-w-sm mx-auto my-16">
+            <div className="mb-10">
+                <label htmlFor="sheetId" className="text-left block mb-2 text-lg font-medium text-gray-900 dark:text-white">Enter google sheet id</label>
+                <input 
+                    id="sheetId" 
+                    className="block p-2.5 w-full text-md text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" 
+                    placeholder="1UEzR3FG9Jk6Vl_2R..."
+                    value={sheetId}
+                    onChange={(e) => setSheetId(e.target.value)}
+                ></input>
+            </div>
             <div className="mb-10">
                 <label htmlFor="emailKeywords" className="text-left block mb-2 text-lg font-medium text-gray-900 dark:text-white">Enter keywords to search for in your Gmail</label>
                 <textarea 
