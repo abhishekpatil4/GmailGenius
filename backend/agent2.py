7f0dd37492f106cc88faca2274765eec5675b297
Abhishek Patil <abishkpatil@gmail.com>
2024-08-24 12:49:25 +0530

    feat: add feat to store attributes form attachment to sheets | add counter.json

--- backend/agent2.py
+++ backend/agent2.py
@@ -15,6 +15,31 @@ load_dotenv()
 
 llm = ChatOpenAI(model="gpt-4o")
 
+#Get the current counter value
+def read_counter():
+    try:
+        with open("counter.json", "r") as file:
+            data = json.load(file)
+            return data["counter"]
+    except FileNotFoundError:
+        initial_data = {"counter": 0}
+        with open("counter.json", "w") as file:
+            json.dump(initial_data, file)
+        return 0
+
+#Tool to increment counter value
+class incrementCounter(BaseTool):
+    name: str = "Increment Counter"
+    description: str = "This tool increments the counter value"
+
+    def _run(self):
+        current_value = read_counter()
+        new_value = current_value + 1
+        data = {"counter": new_value}
+        with open("counter.json", "w") as file:
+            json.dump(data, file)
+
+#Get the attachment that was recently downloaded
 def get_recent_attachment() -> str:
     pdf_files = glob.glob(os.path.join("/Users/abhishekpatil/.composio/output/", "*.pdf")) #modify path as per need
     if not pdf_files:
@@ -23,6 +48,7 @@ def get_recent_attachment() -> str:
     most_recent_pdf = max(pdf_files, key=os.path.getctime)
     return most_recent_pdf
 
+#Extract useful attributes from attachment
 class extractorTool(BaseTool):
     name: str = "ExtractorTool"
     description: str = "This tool extracts useful attributes from pdf document/attachments"
@@ -34,29 +60,33 @@ class extractorTool(BaseTool):
         response = requests.post(url, auth=requests.auth.HTTPBasicAuth(os.environ.get("NANO_API_KEY"), ''), files=FilePath)
         return json.loads(response.text)["result"][0]["prediction"]
 
-
+#Tools
+IncrementCounter = incrementCounter()
 Extractor_tool=extractorTool()
 composio_toolset = ComposioToolSet()
-gmail_tools = composio_toolset.get_actions(
+google_tools = composio_toolset.get_actions(
     actions=[
-        # Action.GMAIL_FETCH_MESSAGE_BY_THREAD_ID,
+        # Action.GMAIL_FETCH_MESSAGE_BY_THREAD_ID, 
         Action.GMAIL_GET_ATTACHMENT,
-        Action.GMAIL_FETCH_EMAILS
+        Action.GMAIL_FETCH_EMAILS,
+        Action.GOOGLESHEETS_BATCH_UPDATE
     ]
 )
-tools = gmail_tools + [Extractor_tool]
+tools = google_tools + [Extractor_tool, IncrementCounter]
 
-gmail_assistant = Agent(
+#Agent
+google_assistant = Agent(
     role="Gmail Assistant",
     # goal="""Find emails/messages using thread id, download attachments & extract attributes/information from it""",
-    goal="""Get recent 5 emails/messages and check if thread id matches, download attachments & extract attributes/information from it""",
-    backstory=f"""You're an AI assistant that makes use of google_tools/APIs and does work on behalf of user. You can extract attributes/information from attachments""",
+    goal="""Get recent 5 emails/messages and check if thread id matches, download attachments & extract attributes/information from it & store attributes in google sheet (Store just the values & not the attribute names)""",
+    backstory=f"""You're an AI assistant that makes use of google tools/APIs and does work on behalf of user. You can extract attributes/information from attachments & Store in google sheet""",
     verbose=True,
     llm=llm,
     tools=tools,
     allow_delegation=False,
 )
 
+#Trigger instance
 listener = composio_toolset.create_trigger_listener()
 
 @listener.callback(filters={"trigger_name": "GMAIL_NEW_GMAIL_MESSAGE"})
@@ -66,18 +96,19 @@ def callback_new_message(event: TriggerEventData) -> None:
     thread_id = payload.get("threadId")
     keywords = "Invoice, Apple TV"
     attributes = "Invoice date, invoice number, invoice amount, currency"
+    sheetId = "1UEzR3FG9Jk6Vl_2RvTgHJuDbuzZXHDMl6yAIX3NDwJU"
+    sheetName = "Sheet1"
     
-    # Correct the description by concatenating strings
     find_invoice_from_gmail = Task(
         description=f"""
-        Find emails with thread id {thread_id}, check if it contains keywords like {keywords}, if so then extract attachment id & attachment name associated with that email and download the attachment & display the following attributes: {attributes} 
+        Find emails with thread id {thread_id}, check if it contains keywords like {keywords}, if so then extract attachment id & attachment name associated with that email and download the attachment & store the following attributes: {attributes} in google sheet with id {sheetId} & sheet name {sheetName} and cell A{read_counter()}
         """,
-        agent=gmail_assistant,
-        expected_output="If email matches criteria ({keywords}) then download attachment & display attributes, otherwise indicate email isnt related",
+        agent=google_assistant,
+        expected_output="If email matches criteria ({keywords}) then download attachment & store attributes on google sheet & increment counter, otherwise indicate email isnt related",
     )
 
     gmail_processing_crew = Crew(
-        agents=[gmail_assistant],
+        agents=[google_assistant],
         tasks=[find_invoice_from_gmail],
         verbose=1,
         process=Process.sequential,
