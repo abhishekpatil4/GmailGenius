7f7ea7bd42f21d135475d35ee4f1f099dd4c14d8
Abhishek Patil <abishkpatil@gmail.com>
2024-08-25 19:05:45 +0530

    feat: created name into component & add logo

---
+++ src/components/LogoComponent.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom"
+import Logo from "../assets/logo.svg";
+const LogoComponent = () => {
+    return <Link to="/" className="flex items-center space-x-1 rtl:space-x-reverse">
+        <img src={Logo} className="logo" alt="GmailGenius logo" width={40} />
+        <span className="self-center text-3xl font-semibold whitespace-nowrap dark:text-white">GmailGenius</span>
+    </Link>
+}
+
+export default LogoComponent;
