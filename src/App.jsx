3ab318790ffc7045f56928ae205b550e5d4a3796
Abhishek Patil <abishkpatil@gmail.com>
2024-08-25 19:02:33 +0530

    feat: protected routes | feat to check for auth changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,57 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "./config/firebase";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import Dashboard from "./pages/Dashboard";
 import ScrollToTop from "./components/ScrollToTop";
+import { useState, useEffect } from "react";
+import Login from "./pages/Login";
+
+const ProtectedRoute = ({ user, children }) => {
+  if (!user) {
+    return <Navigate to="/login" replace />;
+  }
+  return children;
+};
 
 const App = () => {
-  return <>
-    <Navbar />
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      setLoading(false);
+    });
+
+    return () => unsubscribe();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>; // Or any loading indicator
+  }
+
+  return (
     <BrowserRouter>
-    <ScrollToTop />
+      <Navbar user={user} />
+      <ScrollToTop />
       <Routes>
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/" element={<Home />}>
-        </Route>
+        <Route path="/login" element={<Login />} />
+        <Route 
+          path="/dashboard" 
+          element={
+            <ProtectedRoute user={user}>
+              <Dashboard user={user} />
+            </ProtectedRoute>
+          } 
+        />
+        <Route path="/" element={<Home />} />
       </Routes>
+      <Footer />
     </BrowserRouter>
-    <Footer />
-  </>
+  );
 }
 
 export default App;
