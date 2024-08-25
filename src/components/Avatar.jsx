ece8f3eda5a6d73287d21c0a531b54944ccc9b12
Abhishek Patil <abishkpatil@gmail.com>
2024-08-25 19:06:36 +0530

    feat: add avatar dropdown

---
+++ src/components/Avatar.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react';
+import { Dropdown } from 'flowbite';
+import { logOut } from '../config/firebase';
+const Avatar = ({ user }) => {
+    useEffect(() => {
+        if (user) {
+            const $targetEl = document.getElementById('userDropdown');
+            const $triggerEl = document.getElementById('avatarButton');
+
+            if ($targetEl && $triggerEl) {
+                new Dropdown($targetEl, $triggerEl, {
+                    placement: 'bottom-start',
+                    triggerType: 'click'
+                });
+            }
+        }
+    }, [user]);
+
+    if (!user) return null;
+
+    return (
+        <div>
+            <img
+                id="avatarButton"
+                type="button"
+                className="w-8 h-8 rounded-full cursor-pointer"
+                src={user.photoURL}
+                alt="User dropdown"
+            />
+            <div id="userDropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-48 dark:bg-gray-700 dark:divide-gray-600">
+                <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
+                    <div>{user.displayName}</div>
+                    <div className="font-medium truncate">{user.email}</div>
+                </div>
+                <ul className="py-2 text-sm text-gray-700 dark:text-gray-200" aria-labelledby="avatarButton">
+                    <li>
+                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
+                    </li>
+                    <li>
+                        <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Settings</a>
+                    </li>
+                </ul>
+                <div className="py-1">
+                    <a href="#" className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white" onClick={logOut}>Sign out</a>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Avatar;
