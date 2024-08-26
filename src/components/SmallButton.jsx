9c7b37f21ecd9553ce5dce4c00550560dde53b91
Abhishek Patil <abishkpatil@gmail.com>
2024-08-26 18:59:54 +0530

    feat: minor improvements

--- src/components/SmallButton.jsx
+++ src/components/SmallButton.jsx
@@ -1,11 +1,12 @@
-const SmallButton = ({ name, action, color }) => {
+const SmallButton = ({ name, action, color, width = "4.5rem" }) => {
     const handleClick = () => {
         console.log("button clicked");
     }
     return <button
-        onClick={handleClick}
+        style={{ width: width }}
+        onClick={action ? action : handleClick}
         type="button"
-        class="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5">
+        class='focus:outline-none text-white bg-purple-700 hover:bg-purple-800 font-medium rounded-lg text-sm px-5 py-2.5 h-[2.4rem]'>
         {name}
     </button>
 }
